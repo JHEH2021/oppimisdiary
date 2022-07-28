5ba39c8c59ed82fcb2e0a2f671f5bdc1bd16899a
JohannaEH <hyytinen.johanna@gmail.com>
2022-07-28 20:59:35 +0300

    pieniä muutoksia

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -13,32 +13,6 @@ poistaNappi.addEventListener('submit', (event) =>  {
 });
 
 function annaOhjeita() {
-    alert("Tällä sivuilla voit seurata oppimistasi ja asettaa tavoiteita. \nALOITA UUSI AIHE voit syöttää lomakkeelle sen pyytämiä tietoja ja tallentaa ne. \nVoit myös palata tarkistamaan ja muokkaamaan niitä myöhemmin. \n\"KIRJOITA MUISTIINPANO\" voi lisätä matkan varrella ideoitasi ja ajatuksiasi liittyen jonkin aiheen opiskeluun.");
+    alert("Tällä sivuilla voit seurata oppimistasi ja asettaa tavoiteita. \nALOITA UUSI AIHE: voit syöttää lomakkeelle sen pyytämiä tietoja ja tallentaa ne. \nVoit myös palata tarkistamaan ja muokkaamaan niitä myöhemmin. \nKIRJOITA MUISTIINPANO: voi lisätä matkan varrella ideoitasi ja ajatuksiasi liittyen jonkin aiheen opiskeluun.");
 }
 
-var count = 0;
-function changeColor() {
-    setTimeout("showColorChange()", 500);
-}
-function showColorChange() {
-    var bg = new Array("red", "darkblue", "sky", "yellow", "blue", "pink", "green");
-    var txt = new Array("black", "white", "green", "blue", "pink", "red", "yellow");
-    if (count <= 6) {
-        document.getElementById("divChangeColor").style.backgroundColor = bg[count++];
-        document.getElementById("txtChangeColor").style.color = txt[count++];
-        setTimeout("showColorChange()", 500);
-    }
-    else {
-        count = 0;
-        changeColor();
-    }
-}
-function changeBackground(color) {
-    document.getElementsByClassName("form-control").style.background = color;
-}
-
-document.addEventListener("onmouseover", function () { changeBackground('gray') });
-
-function bgChange(bg) {
-    document.getElementsByClassName("form-control" ).style.background = bg;
-}
