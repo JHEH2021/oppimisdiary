cdac01e322ef81c1a8a7ef349f86789390cd9b81
JohannaEH <hyytinen.johanna@gmail.com>
2022-08-13 16:09:33 +0300

    commitoidaan jotain mitkä vielä commitoimatta

---
+++ Migrations/20220804110033_päivitys.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Oppimispäiväkirja_versio1.Migrations
+{
+    public partial class päivitys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Models/ViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oppimispäiväkirja_versio1.Models
+{
+    public class ViewModels
+    {
+        [NotMapped] public List<Topic> Topics { get; set; }
+        [NotMapped] public List<Note> Notes { get; set; }
+        [NotMapped] public Topic Topic { get; set; }
+        [NotMapped] public Note Note { get; set; }
+    }
+}
